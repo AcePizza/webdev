3d208fdc4437fd1d5f390fee0ea2d19da5db1f3c
AcePizza <stianchrbakke@gmail.com>
2022-06-16 15:11:47 +0200

    made the rough outline

---
+++ JavaScript/DOM_Shopping_List/main.js
@@ -0,0 +1,7 @@
+const uList = document.querySelector('ul');
+const inputF = document.querySelector('input');
+const subButton = document.querySelector('button');
+
+function buttonPress() {
+    const pressB = document.querySelector('button').click();
+}
