be9174cd4d23eed0d14e802763bb382e13148337
AcePizza <stianchrbakke@gmail.com>
2022-06-17 17:19:49 +0200

    small changes

--- HTMLandCSS/Semantic_HTML/main.js
+++ HTMLandCSS/Semantic_HTML/main.js
@@ -1,9 +1,12 @@
 let params = (new URL(document.location)).searchParams;
 let firstName = params.get('first-name'); 
 let lastName = params.get('last-name');
-let animal = [params.get('animal')]; 
+let animal = params.getAll('animal'); 
 console.log(animal);
 
+function submitButton() {
+    console.log("did something just happen?");
+}
 
 
 // http://127.0.0.1:5500/HTMLandCSS/Semantic_HTML/form.html?first-name=stian&last-name=bakke&animal=cat&animal=snake&gender=man&contry=Finland
