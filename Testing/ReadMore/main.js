22f8489ee0eb31859bf241030de3db990ff0faed
AcePizza <stianchrbakke@gmail.com>
2022-06-27 10:11:58 +0200

    added working read more/less code

--- Testing/ReadMore/main.js
+++ Testing/ReadMore/main.js
@@ -1,26 +1,50 @@
-let textStuff =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+// THIS WORKS //
+
+const origiText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta."
+
+const spiltStrAt = 200;
 
 document.getElementById("hide-text").addEventListener("click", showHide);
 
-function addText() {
-  let textBoxContainer = document.getElementById("text-box-container");
-  let textBoxOne = document.createElement("p");
-  textBoxOne.innerHTML = textStuff;
-  textBoxContainer.appendChild(textBoxOne);
+let readLessText = origiText.slice(0, spiltStrAt);
+let readMoreText = origiText.slice(spiltStrAt)
+
+// console.log(readLess);
+// console.log(readMore);
+
+function readMoreLess () {
+  let mainContent = document.getElementById('text-box-container')
+  let p = document.createElement('p');
+  p.innerHTML = readLessText;
+
+  let dots = document.createElement("span");
+  dots.setAttribute("id", "dots")
+  dots.innerHTML = "...";
+  
+  let readMore = document.createElement("span");
+  readMore.setAttribute("id", "more");
+  readMore.innerHTML = readMoreText;
+
+  mainContent.appendChild(p);
+  p.appendChild(dots);
+  p.appendChild(readMore);
 }
 
-for (let i = 0; i < 3; i++) {
-    addText();
-}
+readMoreLess();
 
 function showHide () {
-    let x = document.getElementById("text-box-container");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none"
-    }
-}
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("hide-text");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
 
