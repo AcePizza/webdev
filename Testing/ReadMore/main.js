b0d14c6463c820f8429ea3da74c156c23280ad77
AcePizza <stianchrbakke@gmail.com>
2022-06-28 15:43:59 +0200

    changed the size of the string to read

--- Testing/ReadMore/main.js
+++ Testing/ReadMore/main.js
@@ -2,7 +2,7 @@
 
 const origiText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta."
 
-const spiltStrAt = 200;
+const spiltStrAt = 30;
 
 document.getElementById("hide-text").addEventListener("click", showHide);
 
