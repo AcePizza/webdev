c2b5f92732f88f017c32d89e675c657fd006bec3
AcePizza <stianchrbakke@gmail.com>
2022-06-28 15:43:28 +0200

    minor changes

--- Testing/Bootstrap_collaps/main.js
+++ Testing/Bootstrap_collaps/main.js
@@ -4,21 +4,24 @@ let p = document.createElement("p");
 let button = document.createElement("button");
 button.setAttribute("class", "btn btn-primary");
 button.setAttribute("type", "button");
-button.setAttribute("data-toggle", "collapse");
-button.setAttribute("data-target", "#collapseExample");
+button.setAttribute("data-bs-toggle", "collapse");
+button.setAttribute("data-bs-target", "#collapseExample");
 button.setAttribute("aria-expanded", "false");
 button.setAttribute("aria-controls", "collapseExample");
-button.innerHTML = "Button with target";
+button.innerHTML = "Open";
 
 let divCollaps = document.createElement("div");
-divCollaps.setAttribute("class", "collapse")
-divCollaps.setAttribute("id", "collapseExample")
+divCollaps.setAttribute("class", "collapse");
+divCollaps.setAttribute("id", "collapseExample");
 
-let divCardBody = document.createElement("div")
-divCardBody.setAttribute("class", "card card-body")
-divCardBody.innerHTML = "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.";
+let divCardBody = document.createElement("div");
+divCardBody.setAttribute("class", "card card-body");
+divCardBody.innerHTML =
+  "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.";
 
-mainContent.appendChild(p)
-p.appendChild(button)
-mainContent.appendChild(divCollaps)
-divCollaps.appendChild(divCardBody)
+mainContent.appendChild(p);
+p.appendChild(button);
+mainContent.appendChild(divCollaps);
+divCollaps.appendChild(divCardBody);
+
+
