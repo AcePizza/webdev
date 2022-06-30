29ad6fc9c8b625245c9b8a3b9f0f3d250e38038c
AcePizza <stianchrbakke@gmail.com>
2022-06-30 10:28:20 +0200

    fixed so the text initalially shows

--- Testing/Bootstrap_collaps/main.js
+++ Testing/Bootstrap_collaps/main.js
@@ -1,3 +1,6 @@
+const txtCont =
+  "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.";
+
 let mainContent = document.getElementById("main-content");
 let p = document.createElement("p");
 
@@ -7,23 +10,21 @@ button.setAttribute("class", "btn btn-primary");
 button.setAttribute("type", "button");
 button.setAttribute("data-bs-toggle", "collapse");
 button.setAttribute("data-bs-target", "#collapseExample");
-button.setAttribute("aria-expanded", "false");
-button.setAttribute("aria-controls", "collapseExample");
 button.innerHTML = "Read more";
 
 let divCollaps = document.createElement("div");
 divCollaps.setAttribute("class", "collapse");
 divCollaps.setAttribute("id", "collapseExample");
 
-let divCardBody = document.createElement("div");
-divCardBody.setAttribute("class", "card card-body");
-divCardBody.innerHTML =
-  "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.";
+let pTextBox = document.createElement("p");
+pTextBox.setAttribute("class", "card-text");
+pTextBox.innerHTML = txtCont;
 
+mainContent.appendChild(divCollaps);
+divCollaps.appendChild(pTextBox);
 mainContent.appendChild(p);
 p.appendChild(button);
-mainContent.appendChild(divCollaps);
-divCollaps.appendChild(divCardBody);
+
 
 const myCollapsible = document.getElementById("collapseExample");
 myCollapsible.addEventListener("hide.bs.collapse", (event) => {
